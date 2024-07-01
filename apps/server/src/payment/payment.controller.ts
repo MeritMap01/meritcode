4252f7aa1fd8035c77c14f7aa8edc446d8f91069
vamsimeesala789@gmail.com <vamsimeesala789@gmail.com>
2024-07-01 13:06:23 +0530

    new pricing integration

--- apps/server/src/payment/payment.controller.ts
+++ apps/server/src/payment/payment.controller.ts
@@ -1,35 +1,42 @@
+import { Body, Controller, Post, Req, UseGuards } from "@nestjs/common";
+import { Request as ExpressRequest } from "express";
 
+import { JwtGuard } from "../auth/guards/jwt.guard";
+import { RazorPayService } from "./payment.service";
 
-import { Controller, Post, Body, Req, UseGuards } from '@nestjs/common';
-import { Request as ExpressRequest } from 'express';
-import { RazorPayService } from './payment.service';
-import { JwtGuard } from '../auth/guards/jwt.guard';
-
-@Controller('payment')
+@Controller("payment")
 @UseGuards(JwtGuard)
 export class RazorPayController {
-	constructor(private readonly razorpayService: RazorPayService) { }
+  constructor(private readonly razorpayService: RazorPayService) { }
 
-	@Post('create-order')
-	async createOrder(@Body() orderData: { amount: number }, @Req() req: ExpressRequest & { user: { id: string } },) {
-		console.log("hello")
-		const userId = req.user?.id
-		console.log(req.user.id)
-		if (userId) {
-			const order = await this.razorpayService.createOrder(orderData.amount, userId);
-			console.log(order)
-			return order;
-		} else {
-			console.log("Please Login")
-		}
+  @Post("create-order")
+  async createOrder(
+    @Body() orderData: { amount: number },
+    @Req() req: ExpressRequest & { user: { id: string } },
+  ) {
+    console.log("hello");
+    const userId = req.user?.id;
+    console.log(req.user.id);
+    if (userId) {
+      const order = await this.razorpayService.createOrder(orderData.amount, userId);
+      console.log(order);
+      return order;
+    } else {
+      console.log("Please Login");
+    }
+  }
 
-	}
-
-	@Post('verify')
-	async verifyPayment(@Body() paymentData: { razorpay_order_id: string; razorpay_payment_id: string; razorpay_signature: string }) {
-		const verificationStatus = await this.razorpayService.verifyPayment(paymentData);
-		console.log(verificationStatus)
-		return { status: verificationStatus };
-	}
-
+  @Post("verify")
+  async verifyPayment(
+    @Body()
+    paymentData: {
+      razorpay_order_id: string;
+      razorpay_payment_id: string;
+      razorpay_signature: string;
+    },
+  ) {
+    const verificationStatus = await this.razorpayService.verifyPayment(paymentData);
+    console.log(verificationStatus);
+    return { status: verificationStatus };
+  }
 }
--- apps/server/src/payment/payment.module.ts
+++ apps/server/src/payment/payment.module.ts
@@ -1,17 +1,14 @@
 import { Module } from "@nestjs/common";
-import { RazorPayService } from "./payment.service";
-import { RazorPayController } from "./payment.controller";
+import { ConfigModule } from "@nestjs/config";
 import { PrismaModule, PrismaService } from "nestjs-prisma";
-import { ConfigModule, ConfigService } from "@nestjs/config";
 
+import { RazorPayController } from "./payment.controller";
+import { RazorPayService } from "./payment.service";
 
 @Module({
-	imports: [
-		ConfigModule,
-		PrismaModule
-	], providers: [RazorPayService, PrismaService],
-	controllers: [RazorPayController],
-	exports: [RazorPayService],
+  imports: [ConfigModule, PrismaModule],
+  providers: [RazorPayService, PrismaService],
+  controllers: [RazorPayController],
+  exports: [RazorPayService],
 })
-
 export class RazorPayModule { }
