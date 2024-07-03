50846f18015d792ba6c17a18edfd5eecce2d83af
vamsimeesala789@gmail.com <vamsimeesala789@gmail.com>
2024-07-03 10:34:25 +0530

    made changes on equinox and aurora

--- apps/artboard/src/templates/index.tsx
+++ apps/artboard/src/templates/index.tsx
@@ -36,6 +36,7 @@ import { Vintage } from "./vintage";
 import { Sleek } from "./sleek";
 import { Simple } from "./simple";
 import { Designer } from "./designer";
+import { Equinox } from "./equinox";
 
 
 
@@ -114,6 +115,8 @@ export const getTemplate = (template: Template) => {
       return Simple;
     case "designer":
       return Designer;
+    case "equinox":
+      return Equinox;
     default:
       return Onyx;
   }
