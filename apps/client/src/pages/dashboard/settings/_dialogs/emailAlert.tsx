76cd7ee55964bde3eb1a69d72e429083626336f4
Chandan DC <148685726+Chandandc@users.noreply.github.com>
2024-07-17 12:23:53 +0530

    Feat footer navigation static v2 (#66)
    * Added email Verification alert and emailVerification guard
    
    * alignment issues (#53)
    
    * Add changes on logo location and removed negative logic
    
    * Added navigation links in footer section
    
    * feat: taking viveks latest pull
    
    ---------
    
    Co-authored-by: Meesala-vamsi <vamsimeesala789@gmail.com>
    Co-authored-by: SIMHADRI VENKAT VIVEK <120968489+Venkat-Vivek@users.noreply.github.com>

---
+++ apps/client/src/pages/dashboard/settings/_dialogs/emailAlert.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from "react"
+
+interface EmailAlertProps {
+  onClose:()=>void
+}
+
+export const EmailAlert:React.FC<EmailAlertProps>=({onClose})=>{
+
+  useEffect(()=>{
+    const timer = setTimeout(()=>{
+      onClose();
+    },5000)
+  },[onClose])
+
+  return(
+    <div className="fixed flex items-center justify-center inset-0 z-50 bg-black bg-opacity-30">
+      <div className="text-black rounded-md flex flex-col gap-y-10 items-center py-9 px-5 md:w-[40%] h-[50%] shadow-2xl bg-white">
+        <img src="/backgrounds/emailVerify-logo.svg" alt="email-alert" className="h-full" />
+        <h1 className="text-lg md:text-2xl font-bold">Let's get your email verified to get started.</h1>
+      </div>
+    </div>
+  )
+}
+
